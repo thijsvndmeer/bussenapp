846a4ebb395aa4db9fc9c50b5dce6ba2aac15387
thijsvndmeer <thijsvandermeer@outlook.com>
2025-11-28 17:55:22 +0100

    Ensure GitHub Pages entrypoint works

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    const base = '/bussenapp/';
+    const base = env.VITE_BASE_PATH || (mode === 'development' ? '/' : '/bussenapp/');
     return {
       base,
       server: {
