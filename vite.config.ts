e306d714d0773001843ae9f59d3523418c3ae68e
thijsvndmeer <thijsvandermeer@outlook.com>
2025-11-28 17:42:17 +0100

    pages fix2?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    const base = '/bussen-companion/';
+    const base = '/bussenapp/';
     return {
       base,
       server: {
